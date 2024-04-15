9f9f0cb6749ac04ed39555055eae7e9fc27e1c3c
I_D0nt_Know <mayank03ch@gmail.com>
2024-04-15 19:23:56 +0530

    try4

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -20,7 +20,7 @@ export default function Home() {
                 setLoading(true)
             }
         })()
-    }, [loading])
+    }, [setLoading])
 
     return(
         <main className="flex w-full flex-grow">
