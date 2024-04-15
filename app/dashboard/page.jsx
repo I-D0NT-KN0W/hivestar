7dce5641b9f6027a41b9b1bfe1c8182053c99f6e
I_D0nt_Know <mayank03ch@gmail.com>
2024-04-15 19:20:05 +0530

    try2

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -6,7 +6,7 @@ import Post from "@/components/Post";
 
 export default function Home() {
 
-    const [posts, setPosts] = useState<any[]>([]);
+    const [posts, setPosts] = useState([]);
     const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
